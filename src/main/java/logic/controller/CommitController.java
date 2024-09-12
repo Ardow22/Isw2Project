2a1e584d1cbc46ff930a156fdd18fff07319a0a1
Ardow22 <73550813+Ardow22@users.noreply.github.com>
2024-09-12 02:11:24 +0200

    Update CommitController.java

--- src/main/java/logic/controller/CommitController.java
+++ src/main/java/logic/controller/CommitController.java
@@ -139,7 +139,7 @@ public class CommitController {
 				}
 			}
 		    } catch(ArrayIndexOutOfBoundsException e) {
-                e.printStackTrace();
+                        return null;
 		     }
 		return modifiedClasses;
 	    }
